8f919cc6507b4669bc3d6066e2f12f1236030918
VitaliyOne <vitaliy.one1@yandex.ru>
2023-09-01 11:19:43 +0300

    Add a bot data entry form, adding logic and styles

--- src/js/index.js
+++ src/js/index.js
@@ -1,77 +1,198 @@
 // скрипты
-const url = 'https://api.telegram.org/bot6525982560:AAFDLrlGgnm3_S4tzx3RMYJIOUfg0cHT0OI/sendMessage?chat_id=951317487&text=';
+//const botToken = 'bot6525982560:AAFDLrlGgnm3_S4tzx3RMYJIOUfg0cHT0OI';
+//const botChatId = '951317487';
+const telegramApiUrl = 'https://api.telegram.org/';
+let botToken = localStorage.getItem('localBotToken');
+let botChatId = localStorage.getItem('localBotChatId');
+let isBotFormInValid = false;
+let isFormInValid = false;
+
+window.onload = function(){
+  if (botToken && botChatId) {
+    const tokenInput = document.getElementById("token");
+    if (tokenInput) {
+      tokenInput.value = `${botToken}`;
+    }
+    const chaIdInput = document.getElementById("chatId")
+    if (chaIdInput) {
+      chaIdInput.value = `${botChatId}`;
+    }
+    hideBotDataForm();
+    showFeedBackForm();
+  }
+}
 
 submit.onclick = function(event) {
   if ( validateForm()) {
-  const check = checkEvaluation(document.querySelector('input[name="rating"]:checked').value);
-  let checkNew = '';
-  if (check === undefined) { checkNew = 'не выбрана'
-  } else {checkNew = check
-  }
-  const api = `Имя:  ${UserFirstName.value} ${UserLastName.value} %0AОценка:  ${checkNew} %0AЭлектронная почта:  ${UserEmail.value} %0AСообщение:  ${encodeURIComponent(Feedback.value)}`;
-  fetch(url+api)
-  .then(response => responsOut(response.status))
-  .catch(err => alert(`Ошибка отправки`))
-  } else {
-    return console.log('Форма не отправлена')
+    const ratingInput = document.querySelector('input[name="rating"]:checked');
+    const ratingText = getRatingText((ratingInput || {}).value);
+    const userFirstNameInput = document.getElementById("userFirstName");
+    const userFirstNameText = (userFirstNameInput || {}).value;
+    const userLastNameIput = document.getElementById("userLastName");
+    const userLastNameText = (userLastNameIput || {}).value;
+    const userEmailImput = document.getElementById("userEmail");
+    const userEmailText = (userEmailImput || {}).value;
+    const messageInput = document.getElementById("message");
+    const messageText = (messageInput|| {}).value;
+
+    const getFeedbackFormMessage = `Имя:  ${userFirstNameText} ${userLastNameText} %0AОценка:  ${ratingText} %0AЭлектронная почта:  ${userEmailText} %0AСообщение:  ${encodeURIComponent(messageText)}`;
+    fetch(telegramApiUrl+botToken+'/sendMessage?chat_id='+botChatId+`&text=`+getFeedbackFormMessage)
+    .then(response => {
+      if (response.status >= 400) {
+        alert('Ошибка на сервере')
+      } else {
+        const submitButton = document.getElementById("submit")
+        if (submitButton) {
+          submitButton.value = 'Спасибо за обращение!';
+          submitButton.style.width='220px';
+        }
+      }
+    })
+    .catch(err => alert(`Ошибка отправки`));
   }
 }
 
-function responsOut(response) {
-  const responseNew = response;
-  if (responseNew >= 400) {
-    alert('Ошибка на сервере')
-    return response
-  } else {
-    document.getElementById("submit").value = 'Спасибо за обращение!';
-    document.getElementById("submit").style.width='220px';
-    return response
+saveBotDataButton.onclick = function(event) {
+  if (validateBotForm()){
+  saveBotData();
+  hideBotDataForm();
+  showFeedBackForm();
   }
 }
 
-function checkEvaluation (evaluation) {
+function saveBotData () {
+    localStorage.setItem('localBotToken', `${token.value}`);
+    localStorage.setItem('localBotChatId', `${chatId.value}`);
+}
+
+botDataFormTitle.onclick = function(event){
+    const formCollapse = document.getElementById('formCollapse')
+    if (formCollapse) {
+      formCollapse.style.display='block';
+    }
+  } 
+
+function getRatingText (evaluation) {
   switch (evaluation) {
     case '1':
-      evaluation = "Очень плохо";
-      return evaluation;
+      return "Очень плохо";
     case '2':
-      evaluation = "Плохо";
-      return evaluation;
+      return "Плохо";
     case '3':
-      evaluation = "Удовлетворительно";
-      return evaluation;
+      return "Удовлетворительно";
     case '4':
-      evaluation = "Хорошо";
-      return evaluation;
+      return "Хорошо";
     case '5':
-      evaluation = "Отлично";
-      return evaluation;
+      return "Отлично";
+    default : //если попадает что-то лишнее
+      return 'не выбрана';
   }
 }
-let keyRed = '';
-function validateForm (){
-  if (`${UserFirstName.value}` ===''||`${UserEmail.value}`==='') {
-    if (`${UserFirstName.value}` ===''){
-    document.getElementById('UserFirstName').style.cssText = "border-color: rgba(255, 0, 0, .7);";
-    }
-    if (`${UserEmail.value}`==='')  {
-      document.getElementById('UserEmail').style.cssText = "border-color: rgba(255, 0, 0, .7);";
-    }
-    alert('Заполните обязательные пункты формы')
-    keyRed = 'on';
-    return false;
-  } else {
-    return true;
-  }
-};
 
-document.querySelectorAll(".textInputeFeedback").forEach(elem => elem.addEventListener("keydown",
+function validateBotForm () {//разбить на функции валидации, подкраски, алерт
+  const tokenInput = document.getElementById('token')
+  const chatIdInput = document.getElementById('chatId')
+  if (tokenInput || chatIdInput) {
+    changingColorBotForm ()
+    return alertFillBotForm ()
+  }
+}
+
+function changingColorBotForm () {
+  const tokenInput = document.getElementById('token')
+  const chatIdInput = document.getElementById('chatId')
+  if (tokenInput.value === '') {
+    tokenInput.style.cssText = "border-color: rgba(255, 0, 0, .7)";
+  }
+  if (chatIdInput.value === '') {
+      chatIdInput.style.cssText = "border-color: rgba(255, 0, 0, .7)";
+  }
+}
+
+function alertFillBotForm (){
+  const tokenInput = document.getElementById('token')
+  const chatIdInput = document.getElementById('chatId')
+  if (tokenInput.value === '' || chatIdInput.value === '') {
+    alert('Заполните обязательные поля формы')
+    isBotFormInValid = true;
+    return false
+  } else {
+    return true
+  }
+}
+   
+document.querySelectorAll(".inputeDataEntryField").forEach(elem => elem.addEventListener("keydown",
  () => {
-      if (keyRed === 'on') {
-        if (`${UserEmail.value}` !== '')  {
-        document.getElementById('UserEmail').style.cssText = "border-color: rgba(255, 0, 0, .0);";
-        } else if (`${UserFirstName.value}` !== '') {
-        document.getElementById('UserFirstName').style.cssText = "border-color: rgba(255, 0, 0, .0);";
+      if (isBotFormInValid) {
+        const tokenInput = document.getElementById('token');
+        if (tokenInput && tokenInput.value !== '') {
+          tokenInput.style.cssText = "border-color: rgba(255, 0, 0, .0);";
+        }
+        const chatIdInput =  document.getElementById('chatId');
+        if (chatIdInput && chatIdInput.value !== '') {
+          chatIdInput.style.cssText = "border-color: rgba(255, 0, 0, .0);";
         }
       }
   }));
+
+function validateForm (){
+    const userFirstNameInput = document.getElementById("userFirstName");
+    const userEmailImput = document.getElementById("userEmail");
+    if (userFirstNameInput || userEmailImput) {
+      changingColorForm ();
+      return alertFillForm ();
+    } 
+};
+
+function changingColorForm () {
+  const userFirstNameInput = document.getElementById("userFirstName");
+  const userEmailImput = document.getElementById("userEmail");
+  if (userFirstNameInput.value === '') {
+    userFirstNameInput.style.cssText = "border-color: rgba(255, 0, 0, .7)";
+    }
+if (userEmailImput.value === '') {
+    userEmailImput.style.cssText = "border-color: rgba(255, 0, 0, .7);";
+  }
+}
+
+function alertFillForm (){
+  const userFirstNameInput = document.getElementById("userFirstName");
+  const userEmailImput = document.getElementById("userEmail");
+  if (userFirstNameInput.value === '' || userEmailImput.value === '') {
+    alert('Заполните обязательные поля формы') 
+    isFormInValid = true;
+    return false
+  } else {
+    return true
+  }
+}
+
+
+document.querySelectorAll(".textInputeFeedback").forEach(elem => elem.addEventListener("keydown",
+ () => {
+      if (isFormInValid) {
+        const userEmailInput = document.getElementById('userEmail');
+        const userFirstNameInput =  document.getElementById('userFirstName');
+        if (userEmailInput && userEmailInput.value !== '') {
+          userEmailInput.style.cssText = "border-color: rgba(255, 0, 0, .0);";
+        }
+        if (userFirstNameInput &&  userFirstNameInput.value !== '') {
+          userFirstNameInput.style.cssText = "border-color: rgba(255, 0, 0, .0);";
+        }
+      }
+  }));
+
+  function hideBotDataForm () {
+    const formCollapse = document.getElementById('formCollapse')
+    if (formCollapse) {
+      formCollapse.style.display='none';
+    }
+  }
+  
+  function showFeedBackForm () {
+    const hideFeedbackForm = document.getElementById('hideFeedbackForm')
+    if (hideFeedbackForm) {
+    hideFeedbackForm.classList.add('open');
+    hideFeedbackForm.style.opacity='1';
+    }
+  }
